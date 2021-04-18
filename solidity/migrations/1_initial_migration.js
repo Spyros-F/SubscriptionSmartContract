7fcbe077d05697cd48c37d3d76555a135de7cdd7
Spyros F <spyros.f4@gmail.com>
2021-04-19 00:35:57 +0300

    feat: migration file change

--- solidity/migrations/1_initial_migration.js
+++ solidity/migrations/1_initial_migration.js
@@ -1,5 +1,7 @@
+const SubscriptionContract = artifacts.require("SubscriptionContract");
 const DaiToken = artifacts.require("DAI");
 
 module.exports = function (deployer, networks, accounts) {
-  deployer.deploy(DaiToken,'MYDAI', 'DAI');
+  await deployer.deploy(DaiToken, 'MYDAI', 'DAI')
+  await deployer.deploy(SubscriptionContract, 20, accounts[2], DaiToken.address)
 };
