62e25ae776b56c286542f5dbc0b8c97598f82f94
Spyros F <spyros.f4@gmail.com>
2021-04-20 19:02:34 +0300

    chore: added toWei convertion in subscription size

--- solidity/migrations/1_initial_migration.js
+++ solidity/migrations/1_initial_migration.js
@@ -1,7 +1,10 @@
-const SubscriptionContract = artifacts.require("SubscriptionContract");
-const DaiToken = artifacts.require("DAI");
+const Web3 = require('web3')
 
-module.exports = function (deployer, networks, accounts) {
+const SubscriptionContract = artifacts.require("SubscriptionContract")
+const DaiToken = artifacts.require("DAI")
+web3 = new Web3(new Web3.providers.HttpProvider('https://kovan.infura.io/v3/c4fe3240d0cb4071bf1ab8276b6160fa'))
+
+module.exports = async function (deployer, networks, accounts) {
   await deployer.deploy(DaiToken, 'MYDAI', 'DAI')
-  await deployer.deploy(SubscriptionContract, 20, accounts[2], DaiToken.address)
+  await deployer.deploy(SubscriptionContract, web3.utils.toWei('20','ether'), accounts[2], DaiToken.address)
 };
