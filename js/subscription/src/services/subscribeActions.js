c1d7e898fbcfc3abed90714ba886ba060735119f
Spyros F <spyros.f4@gmail.com>
2021-04-20 19:03:34 +0300

    chore: services fixes

--- js/subscription/src/services/subscribeActions.js
+++ js/subscription/src/services/subscribeActions.js
@@ -1,30 +1,50 @@
-const Web3 = require('web3')
 const {abi} = require('../abi/SubscriptionContract.json')
-require('dotenv').config() 
-console.log(process.env);
+const {getCustomWeb3Provider} = require('./customProvider')
+const {getWeb3Provider} = require('./provider')
+const {createDaiInstance} = require('./daiContractInstance')
 
-const web3 = new Web3(new Web3.providers.HttpProvider('https://kovan.infura.io/v3/c4fe3240d0cb4071bf1ab8276b6160fa')) // TO DO remove it to .env file
-
-const contractAddress = '0x812cA984a3BE1b7041A00FFc16405ad8827c011B'
+const contractAddress = '0xc0846273a17e519c3085f2d07099D87a3fdC908f'
 
 export const getSizeOfSubscription = async() => {
+  const web3 = getCustomWeb3Provider()
   const subscriptionContract = new web3.eth.Contract(abi, contractAddress)
   const sizeOfSubscription =  await subscriptionContract.methods.getSizeOfSubscription().call()
-  return sizeOfSubscription
+  return web3.utils.fromWei(sizeOfSubscription,'ether')
 }
 
 export const getUserBalance = async(address) => {
+  const web3 = getCustomWeb3Provider()
   const subscriptionContract = new web3.eth.Contract(abi, contractAddress)
   const userBalance =  await subscriptionContract.methods.balanceOf(address).call()
-  return userBalance
+  return web3.utils.fromWei(userBalance,'ether')
 }
 
 export const getLastMonthOfSubscription = async(address) => {
+  const web3 = getWeb3Provider()
   const subscriptionContract = new web3.eth.Contract(abi, contractAddress)
   const months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
-  await subscriptionContract.methods.SubscriptionDate(address).call()
-  let lastSubscriptionDate =  await subscriptionContract.methods.getUserSubscriptionDate().call()
-  lastSubscriptionDate = new Date(lastSubscriptionDate * 1000)
-  const lastMonthOfSubscription = months[lastSubscriptionDate.getMonth()]
-  return lastMonthOfSubscription
+  let lastSubscriptionDate =  await subscriptionContract.methods.getUserSubscriptionDate(address).call()
+  if (lastSubscriptionDate != 0) {
+    lastSubscriptionDate = new Date(lastSubscriptionDate * 1000)
+    const lastMonthOfSubscription = months[lastSubscriptionDate.getMonth()]
+    return lastMonthOfSubscription
+  }
+  return ''
+}
+
+export const depositAction = async(amount, account) => {
+  const web3 = getWeb3Provider()
+  const Web3 = getCustomWeb3Provider()
+  const subscriptionContract = new web3.eth.Contract(abi, contractAddress)
+  const daiInstance = await createDaiInstance()
+  await daiInstance.methods.mint(account, Web3.utils.toWei(amount,'ether')).send({from: account})
+  await daiInstance.methods.increaseAllowance(contractAddress,  Web3.utils.toWei(amount,'ether')).send({from:account})
+  await subscriptionContract.methods.deposit( Web3.utils.toWei(amount,'ether')).send({from: account})
+}
+
+export const isUserSubscribed = async(account) => {
+  const web3 = getWeb3Provider()
+  const subscriptionContract = new web3.eth.Contract(abi, contractAddress)
+  const isUserSubscribed = await subscriptionContract.methods.getIsUserSubscribed(account).call()
+  return isUserSubscribed
 }
